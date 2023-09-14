606c56c7f24de95d3c71b2fad15380b0ad656b0f
Ian Alves <ian.santos.alves@hotmail.com>
2023-09-13 22:50:07 -0300

    adicionando redirecionamento para /hello

--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@ const employeeRoutes = require("./employee/employee.routes");
 const app = express();
 
 app.use(express.json());
-
+app.get("/", async (req, res) => {
+    res.redirect("/hello");
+});
 app.get("/hello", async (req, res) => {
     return res.json({ message: "Hello, Cognum!" });
 });
