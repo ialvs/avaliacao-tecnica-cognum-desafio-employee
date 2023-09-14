7a66cdf62207a47ebba2dd3adfbd24956d3124bf
Ian Alves <ian.santos.alves@hotmail.com>
2023-09-13 21:35:50 -0300

    adicionado middleware para garantir que name e role nao sao nulos

--- src/employee/employee.middleware.js
+++ src/employee/employee.middleware.js
@@ -10,4 +10,16 @@ const paramIdIsNum = async (req, res, next) => {
     next();
 };
 
-module.exports = { paramIdIsNum };
+const nameRoleAreNull = async (req, res, next) => {
+    const { name, role } = req.body;
+
+    if (!name || !role) {
+        return res
+            .status(400)
+            .json({ mensagem: "nome e cargo devem ser informados" });
+    }
+
+    next();
+};
+
+module.exports = { paramIdIsNum, nameRoleAreNull };
