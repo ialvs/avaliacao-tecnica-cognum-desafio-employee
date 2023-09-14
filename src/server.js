ec698da4b6219d0ac178ea2b74631e7e97c01492
Ian Alves <ian.santos.alves@hotmail.com>
2023-09-13 22:32:55 -0300

    adicionando rota /hello

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ const employeeRoutes = require("./employee/employee.routes");
 const app = express();
 
 app.use(express.json());
+
+app.get("/hello", async (req, res) => {
+    return res.json({ message: "Hello, Cognum!" });
+});
+
 app.use("/employee", employeeRoutes);
 
 module.exports = app;
