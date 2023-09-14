d65b9d124f869a72dab1ebf40c3fb38e764bf71a
Ian Alves <ian.santos.alves@hotmail.com>
2023-09-13 21:16:12 -0300

    adicionado controlador para rota GET employee/id

--- src/employee/employee.controller.js
+++ src/employee/employee.controller.js
@@ -1,4 +1,4 @@
-const { findAll } = require("./employee.service");
+const { findAll, findById } = require("./employee.service");
 
 const getAll = async (req, res) => {
     const employees = await findAll();
@@ -6,4 +6,16 @@ const getAll = async (req, res) => {
     return res.json(employees);
 };
 
-module.exports = { getAll };
+const getById = async (req, res) => {
+    const { id } = req.params;
+
+    const employee = await findById(id);
+
+    if (employee.length == 0) {
+        return res.status(404).json({ mensagem: "empregado não encontrado." });
+    }
+
+    return res.json(employee);
+};
+
+module.exports = { getAll, getById };
--- src/employee/employee.routes.js
+++ src/employee/employee.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { getAll } = require("./employee.controller");
+const { getAll, getById } = require("./employee.controller");
+const { paramIdIsNum } = require("./employee.middleware");
 
 const routes = Router();
 
 routes.get("/", getAll);
+routes.get("/:id", paramIdIsNum, getById);
 
 module.exports = routes;
