bc525d4ff00fa58a42e0490cf15666ee14853a72
Ian Alves <ian.santos.alves@hotmail.com>
2023-09-15 19:42:32 -0300

    adicionado cors na configuracao do servidor

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const employeeRoutes = require("./employee/employee.routes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.get("/", async (req, res) => {
     res.redirect("/hello");
 });
+
 app.get("/hello", async (req, res) => {
     return res.json({ message: "Hello, Cognum!" });
 });
