79146c683932f4d2235a84ae4eedaa94fdd9385b
Ian Alves <ian.santos.alves@hotmail.com>
2023-09-15 21:19:16 -0300

    adicionadas funcoes para obter e inserir employee no banco

--- src/employee/employee.service.js
+++ src/employee/employee.service.js
@@ -1,4 +1,5 @@
 const knex = require("../db/knex.connection");
+const axios = require("axios").default;
 
 const findAll = async () => {
     return await knex("employee").orderBy("id");
@@ -29,4 +30,59 @@ const remove = async id => {
     return await knex("employee").where("id", id).del();
 };
 
-module.exports = { findAll, findById, create, update, remove };
+const getRandomPeople = async () => {
+    try {
+        const quantity = 10;
+        const fields = "name";
+
+        const { data } = await axios.get(
+            `https://randomuser.me/api/?results=${quantity}&inc=${fields}`
+        );
+        const { results } = data;
+
+        const fullNames = results.map(person => {
+            return { name: `${person.name.first} ${person.name.last}` };
+        });
+        return fullNames;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+const roleChooser = () => {
+    const randomNumber = Math.floor(Math.random() * (9 - 0 + 1) + 0);
+    const roles = {
+        0: "Sales Manager",
+        1: "Marketing Analyst",
+        2: "Software Engineer",
+        3: "Administrative Assistant",
+        4: "Graphic Designer",
+        5: "HR Analyst",
+        6: "Front-end Developer",
+        7: "Project Manager",
+        8: "Financial Analyst",
+        9: "Quality Analyst",
+    };
+    return roles[randomNumber];
+};
+
+const populateEmployee = async () => {
+    const names = await getRandomPeople();
+
+    const employees = names.map(name => {
+        let role = roleChooser();
+        let onlyName = name.name;
+        return { name: onlyName, role };
+    });
+
+    return knex("employee").insert(employees, ["id", "name", "role"]);
+};
+
+module.exports = {
+    findAll,
+    findById,
+    create,
+    update,
+    remove,
+    populateEmployee,
+};
